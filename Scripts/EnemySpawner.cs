944b5d05a56b31f861fe93fe924281b4f8f5d3ba
genr234 <riccardocavallaro2020@outlook.it>
2025-10-17 12:20:00 +0200

    Add zoom in/out and a simple enemy spawner system

--- Scripts/EnemySpawner.cs
+++ Scripts/EnemySpawner.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
     public List<GameObject> enemies = new List<GameObject> { };
     private void Start()
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     public float speed = 5f;
+    public CameraController cameraController;
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -14,11 +17,12 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        var inputVector = InputSystem.actions.FindAction("Move").ReadValue<Vector2>();
+        var inputVector = InputSystem.actions.FindAction("Player/Move").ReadValue<Vector2>();
         var movement = new Vector3(inputVector.x, 0, inputVector.y) * (speed * Time.fixedDeltaTime);
         var rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
         movement = rotation * movement;
         _rigidbody.MovePosition(_rigidbody.position + movement);
+        cameraController.zoomedOut = InputSystem.actions.FindAction("Player/Zoom Out").IsPressed();
     }
     
 }
